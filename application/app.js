d9eac6ce3aa5a9a306f3c0fbed541c52a0c05ecc
Juha Vepsä <juha.a.vepsa@gmail.com>
2016-02-16 02:38:39 +0200

    added header and footer sections to the web page

--- application/app.js
+++ application/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+// var header = require('.routes/header');
 
 var app = express();
 
